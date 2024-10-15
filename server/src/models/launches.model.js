31d746b82a7565523cca28e5010e2b381d9d1739
Ranjith <ranjith.seekolu@wavemaker.com>
2024-10-15 22:48:06 +0530

    Add nasa api

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,6 @@
 const launches=require('./launches.mongo');
-const planets=require('./planets.mongo')
+const planets=require('./planets.mongo');
+const axios=require('axios');
 // const launches = new Map();
 
 const DEFAULT_FLIGHT_NUBMER=100;
@@ -8,7 +9,7 @@ let latestFlightNumber = 100;
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
-  rocket: "Explore IS1",
+  rocket: "Explore IS1", 
   launchDate: new Date("December 27,2030"),
   target: "Kepler-442 b",
   customers: ["ZTM", "NASA"],
@@ -19,6 +20,51 @@ const launch = {
 // launches.set(launch.flightNumber, launch);
 
 saveLaunch(launch);
+
+const SPACEX_API_URL='https://api.spacexdata.com/v4/launches/query';
+
+async function loadLaunchData(){
+  console.log('Downloading launch data ... ');
+  const response=await axios.post(SPACEX_API_URL,{
+      query:{},
+      options:{
+          populate:[
+              {
+                  path:'rocket',
+                  select:{
+                      name:1
+                  }
+              },
+              {
+                  path:'payloads',
+                  select:{
+                    customers:1
+                  }
+              }
+          ]
+      }
+  });
+
+  const launchDocs=response.data.docs;
+  for(const launchDoc of launchDocs){
+    const payloads=launchDoc['payloads'];
+    const customers=payloads.flatMap((payload)=>{
+      return payload['customers'];
+    })
+    const launch={
+      flightNumber:launchDoc['flight_number'],
+      mission:launchDoc['name'],
+      rocket:launchDoc['rocket']['name'],
+      launchDate:launchDoc['date_local'],
+      upcoming:launchDoc['upcoming'],
+      success:launchDoc['success'],
+      customers:customers
+    }
+
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+  }
+
+}
 
 async function existsLaunchWithId(launchId) {
   return await launches.findOne({
@@ -77,6 +123,7 @@ async function saveLaunch(launch){
 }
 
 module.exports = {
+  loadLaunchData,
   existsLaunchWithId,
   getAllLaunches,
   scheduleNewLaunch,
